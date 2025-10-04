c0acd3d62d45fddc83cf33ef0cff56d30b02d2f6
Liliya Sviatenko <123566371+Liliya-ivanovna@users.noreply.github.com>
2025-10-04 15:23:23 +0300

    й

--- productcard/src/App.vue
+++ productcard/src/App.vue
@@ -1,23 +1,5 @@
 <template>
 <div id="app">
-<ProductList 
-name="Українська вишиванка."
-description="Стильна вишиванка з традиційним орнаментом."
-price="1500"
-image="https://recommerce.com.ua/static/kozachok.reshop.com.ua/catalog/381/195715475364252e14cc6cb_medium.jpg"
-/>
-<ReviewsComponent/>
+<router-view></router-view>
 </div>
 </template>
-
-<script>
-import ProductList from "./components/ProductList.vue";
-import ReviewsComponent from "./components/ReviewsComponent.vue";
-
-export default{
-  components: {
-ProductList,
-ReviewsComponent,
-  }
-}
-</script>
